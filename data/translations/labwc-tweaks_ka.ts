01ecefc17ab715d0f4b58e6170ce0412b695fc01
Weblate <noreply@weblate.org>
2025-01-11 07:12:20 +0100

    Weblate commits
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: vTT <vttfreebsd@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ka/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

---
+++ data/translations/labwc-tweaks_ka.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../src/maindialog.ui" line="57"/>
+        <source>Appearance</source>
+        <translation>გარეგნობა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="118"/>
+        <source>Corner Radius</source>
+        <translation>კუთხის რადიუსი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="108"/>
+        <source>Openbox Theme</source>
+        <translation>Openbox-ის თემა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="65"/>
+        <source>Behaviour</source>
+        <translation>ქცევა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="73"/>
+        <source>Mouse &amp; Touchpad</source>
+        <translation>თაგუნა და თაჩპედი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="128"/>
+        <source>Drop Shadows</source>
+        <translation>ჩრდილები</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="154"/>
+        <source>Placement Policy</source>
+        <translation>მოთავსების პოლიტიკა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="180"/>
+        <source>Cursor Theme</source>
+        <translation>კურსორის თემა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="190"/>
+        <source>Cursor Size</source>
+        <translation>კურსორის ზომა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="200"/>
+        <source>Natural Scroll</source>
+        <translation>ბუნებრივი გადახვევა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="241"/>
+        <source>Add</source>
+        <translation>დამატება</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="248"/>
+        <source>Remove</source>
+        <translation>წაშლა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="81"/>
+        <source>Language &amp; Region</source>
+        <translation>ენა და რეგიონი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="226"/>
+        <source>Keyboard Layout</source>
+        <translation>კლავიატურის განლაგება</translation>
+    </message>
+</context>
+</TS>
